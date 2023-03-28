beefc8b5f660e4921025a1ffdbd0f5bb40bb881c
Dorcas@126 <uwimbabazidorcas65@gmail.com>
2023-03-28 11:09:58 +0000

    Add 0-lockboxes.py

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,19 +1,24 @@
 #!/usr/bin/python3
 """
-Solution to lockboxes problem
+Module for canUnlockAll.
 """
 
 
 def canUnlockAll(boxes):
-    keys = [0]  # list of keys available to the player
-    unlocked = [False] * len(boxes)  # list of boxes unlocked by the player
-    unlocked[0] = True  # player starts with box 0 unlocked
-    
-    while keys:
-        box_idx = keys.pop()
-        for key in boxes[box_idx]:
-            if key >= 0 and key < len(boxes) and not unlocked[key]:
-                keys.append(key)
-                unlocked[key] = True
-    
-    return all(unlocked)
+    """
+    Determines if all the boxes can be opened.
+    """
+    n = len(boxes)
+    seen = [False] * n
+    seen[0] = True
+    queue = [0]
+
+    while queue:
+        box = queue.pop(0)
+        for key in boxes[box]:
+            if key < n and not seen[key]:
+                seen[key] = True
+                queue.append(key)
+
+    return all(seen)
+
